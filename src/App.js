35d1cf236f2761c0d45507347c88e67aeb5d8291
Aliaksandr Bychak <aliaksandr.bychak@gmail.com>
2018-12-10 16:33:13 +0300

    get listDogs

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import ListDogs from './ListDogs'
+
+const LIST_ALL_DOGS = 'https://dog.ceo/api/breeds/list/all'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      // <div className="App">
+      //   <a
+      //     className="App-link"
+      //     href="https://reactjs.org"
+      //     target="_blank"
+      //     rel="noopener noreferrer"
+      //   >
+      //     Learn React
+      //   </a>
+      // </div>
+      <React.Fragment>
+        <ListDogs data={this.getListDogs()} />
+      </React.Fragment>
+    )
+  }
+
+  getListDogs = () => {
+    const data = fetch(LIST_ALL_DOGS).json()
+    console.log(data)
+    return data
   }
 }
 
-export default App;
+export default App
---
+++ src/ListDogs.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class ListDogs extends Component {
+  render() {
+    return <div className="App">Списка нет!</div>
+  }
+}
+
+export default ListDogs

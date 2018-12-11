e5c4f0f2de362fdff5ebdf8f787da87b09735b1a
Aliaksandr Bychak <aliaksandr.bychak@gmail.com>
2018-12-11 13:24:50 +0300

    reconfig app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
 import React, { Component } from 'react'
 import './App.css'
-import ListDogs from './ListDogs'
+import ListDogs from './components/ListDogs'
 
 const LIST_ALL_DOGS = 'https://dog.ceo/api/breeds/list/all'
 
 class App extends Component {
   render() {
     return (
-      // <div className="App">
-      //   <a
-      //     className="App-link"
-      //     href="https://reactjs.org"
-      //     target="_blank"
-      //     rel="noopener noreferrer"
-      //   >
-      //     Learn React
-      //   </a>
-      // </div>
       <React.Fragment>
-        <ListDogs data={this.getListDogs()} />
+        <h1>My React App!</h1>
       </React.Fragment>
     )
   }
 
-  getListDogs = () => {
-    const data = fetch(LIST_ALL_DOGS).json()
-    console.log(data)
-    return data
-  }
+  // getListDogs = () => {
+  //   let listDogs = fetch(LIST_ALL_DOGS)
+  //   const data = listDogs.json()
+  //   console.log(data.message)
+  //   return data
+  // }
 }
 
 export default App
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+var WebpackMd5Hash = require('webpack-md5-hash')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin('dist', {}),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new WebpackMd5Hash(),
+  ],
+}
